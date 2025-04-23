c93a2e0856a9a09bee7330ac950bc9ca0831ac37
Robert Gehrsitz <robertg@digwave.com>
2025-04-23 14:56:37 -0400

    Refactor GenerateCommand to use TemplateManagerV2 for improved template handling

--- Pulsar/Pulsar.Compiler/Commands/GenerateCommand.cs
+++ Pulsar/Pulsar.Compiler/Commands/GenerateCommand.cs
@@ -55,7 +55,7 @@ namespace Pulsar.Compiler.Commands
                 }
 
                 // Generate project files
-                var templateManager = new TemplateManager();
+                var templateManager = new TemplateManagerV2(_logger);
                 templateManager.GenerateProjectFiles(buildConfig.OutputPath, buildConfig);
 
                 return 0;
